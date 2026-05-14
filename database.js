ce24616adc46d86a476b090ad5ac3d92fb57cde6
ar1stov <qwertyarstv@gmail.com>
2026-05-14 09:09:15 +0300

    Update database.js

--- database.js
+++ database.js
@@ -1,17 +1,17 @@
 const database = {
     "Тригонометрия": [
         { 
-            question: "img/trig_q1.png", 
-            solution: "img/trig_sol1.png", 
-            options: ["0", "1", "0.5", "-1"], 
-            answer: "0.5" 
+            question: "img/trig_11.png", 
+            solution: "img/trig_1_resh.png", 
+            answer: "24" 
         }
     ],
     "Планиметрия": [
         { 
             question: "img/plan_q1.png", 
             solution: "img/plan_sol1.png", 
-            answer: "100" 
+            options: ["0", "1", "0.5", "-1"], 
+            answer: "1" 
         }
     ],
     "Стереометрия": [
