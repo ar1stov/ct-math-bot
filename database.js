b05265d901e3d00b041189ead16ffa36db4c6d39
ar1stov <qwertyarstv@gmail.com>
2026-05-14 09:05:43 +0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,24 @@
+const database = {
+    "Тригонометрия": [
+        { 
+            question: "img/trig_q1.png", 
+            solution: "img/trig_sol1.png", 
+            options: ["0", "1", "0.5", "-1"], 
+            answer: "0.5" 
+        }
+    ],
+    "Планиметрия": [
+        { 
+            question: "img/plan_q1.png", 
+            solution: "img/plan_sol1.png", 
+            answer: "100" 
+        }
+    ],
+    "Стереометрия": [
+        { 
+            question: "img/stereo_q1.png", 
+            solution: "img/stereo_sol1.png", 
+            answer: "15" 
+        }
+    ]
+};
